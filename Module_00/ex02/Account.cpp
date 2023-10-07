5d509a580438b891d99a58972df7c93abc799813
ffouquet42 <ffouquet42@gmail.com>
2023-10-07 18:43:39 +0200

    add Makefile and function for display timestamp

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,7 +6,35 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 18:16:56 by fllanet           #+#    #+#             */
-/*   Updated: 2023/10/07 18:16:57 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/10/07 18:43:05 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) // constructor
+{
+	
+}
+
+Account::~Account(void) // destructor
+{
+	
+}
+
+void Account::_displayTimestamp(void) // display timestamp
+{
+	time_t	local_time;
+	char	timestamp[19];
+
+	std::time(&local_time);
+	std::strftime(timestamp, 19, "[%Y%m%d_%H%M%S] ", localtime(&local_time));
+	std::cout << timestamp;
+}
