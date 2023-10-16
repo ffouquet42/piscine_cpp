b344843fadcf6f9bf7f849a4d830545b1fc39317
ffouquet42 <ffouquet42@gmail.com>
2023-10-16 15:58:36 +0200

    adjust indentation

--- Module_01/ex00/includes/Zombie.hpp
+++ Module_01/ex00/includes/Zombie.hpp
@@ -6,14 +6,14 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:58:50 by fllanet           #+#    #+#             */
-/*   Updated: 2023/10/16 15:44:14 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/10/16 15:57:00 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 //---------------   includes   ---------------//
-#include <iostream>
+# include <iostream>
 
 //---------------   class   ---------------//
 class Zombie
@@ -33,10 +33,10 @@ Zombie	*newZombie(std::string name);
 void	randomChump(std::string name);
 
 //---------------   colors   ---------------//
-# define RED "\x1B[31m"
-# define GREEN "\x1B[32m"
-# define YELLOW "\x1B[33m"
-# define BLUE "\x1B[34m"
-# define MAGENTA "\x1B[35m"
-# define CYAN "\x1B[36m"
-# define WHITE "\x1B[0m"
+# define RED		"\x1B[31m"
+# define GREEN		"\x1B[32m"
+# define YELLOW		"\x1B[33m"
+# define BLUE		"\x1B[34m"
+# define MAGENTA	"\x1B[35m"
+# define CYAN		"\x1B[36m"
+# define WHITE		"\x1B[0m"
