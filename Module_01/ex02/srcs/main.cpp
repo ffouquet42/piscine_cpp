ee753cfd8744be70c52284242f701ef76ceb6ad3
ffouquet42 <ffouquet42@gmail.com>
2023-10-18 15:41:00 +0200

    reindentation

--- Module_01/ex02/srcs/main.cpp
+++ Module_01/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:32:05 by fllanet           #+#    #+#             */
-/*   Updated: 2023/10/18 14:38:11 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/10/18 15:40:38 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int	main(void)
 {
 	// variables initialization
 	std::string str = "HI THIS IS BRAIN";
-    std::string *stringPTR = &str;
-    std::string &stringREF = str;
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
 	// print adress memory
-    std::cout << "\x1B[34m" << &str << "\x1B[0m" << std::endl;
-    std::cout << "\x1B[34m" << stringPTR << "\x1B[0m" << std::endl;
-    std::cout << "\x1B[34m" << &stringREF << "\x1B[0m" << std::endl;
+	std::cout << "\x1B[34m" << &str << "\x1B[0m" << std::endl;
+	std::cout << "\x1B[34m" << stringPTR << "\x1B[0m" << std::endl;
+	std::cout << "\x1B[34m" << &stringREF << "\x1B[0m" << std::endl;
 
 	// print string value
-    std::cout << "\x1B[32m" << str << "\x1B[0m" << std::endl;
-    std::cout << "\x1B[32m" << *stringPTR << "\x1B[0m" << std::endl;
-    std::cout << "\x1B[32m" << stringREF << "\x1B[0m" << std::endl;
-	
+	std::cout << "\x1B[32m" << str << "\x1B[0m" << std::endl;
+	std::cout << "\x1B[32m" << *stringPTR << "\x1B[0m" << std::endl;
+	std::cout << "\x1B[32m" << stringREF << "\x1B[0m" << std::endl;
+
 	return (0);
 }
