a48642c25c39063f7caf6e2d59be96bcfe41898c
ffouquet42 <ffouquet42@gmail.com>
2023-10-19 15:28:32 +0200

    need to code function for replace text

---
+++ Module_01/ex04/includes/sed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sed.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 14:40:47 by fllanet           #+#    #+#             */
+/*   Updated: 2023/10/19 15:19:28 by fllanet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+//---------------   includes   ---------------//
+# include <iostream>
+# include <fstream>
+# include <string>
+
+//---------------   colors   ---------------//
+# define RED		"\x1B[31m"
+# define GREEN		"\x1B[32m"
+# define YELLOW		"\x1B[33m"
+# define BLUE		"\x1B[34m"
+# define MAGENTA	"\x1B[35m"
+# define CYAN		"\x1B[36m"
+# define WHITE		"\x1B[0m"
--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -6,7 +6,55 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:35:20 by fllanet           #+#    #+#             */
-/*   Updated: 2023/10/19 14:35:21 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/10/19 15:28:03 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/sed.hpp"
+
+int	replace_text(char **argv)
+{
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	std::ifstream in_file(filename);
+	if (!in_file)
+	{
+		std::cout << RED << "File <" << filename << "> not found." << WHITE << std::endl;
+		return (1);
+	}
+	std::ofstream out_file(filename + ".replace");
+	if (!out_file)
+	{
+		std::cout << RED << "Can't create <" << filename << ".replace>." << WHITE << std::endl;
+		return (1);
+	}
+	
+	//
+	
+	std::cout << GREEN << "A new file <" << filename << ".replace> was created with modifications." << WHITE << std::endl;
+	return (0);
+}
+
+int	check_args(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << RED << "Wrong number of arguments." << WHITE << std::endl;
+		return (1);
+	}
+	if (!argv[2][0])
+	{
+		std::cout << RED << "Text to replace can't be blank." << WHITE << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (check_args(argc, argv))
+		return (1);
+	return replace_text(argv) ? 1 : 0;
+}
