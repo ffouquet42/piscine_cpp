cc22eaa8d70428fb38ab80fe044c3a8a342394b1
ffouquet42 <ffouquet42@gmail.com>
2023-11-15 15:43:08 +0100

    upgrade ex00 by creating includes folder

--- Module_00/ex00/includes/program.hpp
+++ Module_00/ex00/includes/program.hpp
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   program.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 11:34:17 by fllanet           #+#    #+#             */
-/*   Updated: 2023/11/15 15:36:04 by fllanet          ###   ########.fr       */
+/*   Created: 2023/11/15 15:31:16 by fllanet           #+#    #+#             */
+/*   Updated: 2023/11/15 15:33:51 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/program.hpp"
+#pragma once
 
-int main(int argc, char **argv)
-{
-	if (argc < 2)
-		std::cout << YELLOW << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << WHITE;
-	for (int i = 1; argv[i]; i++)
-		for (int j = 0; argv[i][j]; j++)
-			std::cout << GREEN << (char)toupper(argv[i][j]) << WHITE;
-	std::cout << std::endl;
-	return (0);
-}
+//---------------   includes   ---------------//
+#include <iostream>
+
+//---------------   colors   ---------------//
+# define RED		"\x1B[31m"
+# define GREEN		"\x1B[32m"
+# define YELLOW		"\x1B[33m"
+# define BLUE		"\x1B[34m"
+# define MAGENTA	"\x1B[35m"
+# define CYAN		"\x1B[36m"
+# define WHITE		"\x1B[0m"
