8c8ecc7628e9eb6874031bb943dc75a48fb9803a
ffouquet42 <ffouquet42@gmail.com>
2023-11-20 07:41:22 +0100

    cancel colors.hpp

--- Module_00/ex00/includes/program.hpp
+++ Module_00/ex00/includes/program.hpp
@@ -6,13 +6,20 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:31:16 by fllanet           #+#    #+#             */
-/*   Updated: 2023/11/20 07:38:39 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/11/20 07:41:07 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 //---------------   includes   ---------------//
-# include <iostream>
+#include <iostream>
 
-# include "../includes/colors.hpp"
+//---------------   colors   ---------------//
+# define RED		"\x1B[31m"
+# define GREEN		"\x1B[32m"
+# define YELLOW		"\x1B[33m"
+# define BLUE		"\x1B[34m"
+# define MAGENTA	"\x1B[35m"
+# define CYAN		"\x1B[36m"
+# define WHITE		"\x1B[0m"
--- Module_00/ex00/srcs/megaphone.cpp
+++ Module_00/ex00/srcs/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 11:34:17 by fllanet           #+#    #+#             */
-/*   Updated: 2023/11/20 07:35:37 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/11/20 07:40:59 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int main(int argc, char **argv)
 {
 	if (argc < 2)
-		std::cout << COLOR_BRIGHT_RED << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << COLOR_WHITE;
+		std::cout << YELLOW << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << WHITE;
 	for (int i = 1; argv[i]; i++)
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << COLOR_GREEN << (char)toupper(argv[i][j]) << COLOR_WHITE;
+			std::cout << GREEN << (char)toupper(argv[i][j]) << WHITE;
 	std::cout << std::endl;
 	return (0);
 }
