701bd4df756435178697f6dd8e48a6e087bdec79
ffouquet42 <ffouquet42@gmail.com>
2023-11-28 11:10:45 +0100

    ex03 done with lot of tests

--- Module_02/ex03/srcs/bsp.cpp
+++ Module_02/ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 08:15:37 by fllanet           #+#    #+#             */
-/*   Updated: 2023/11/28 09:21:25 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/11/28 11:10:04 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 Fixed	vector(Point const a, Point const b, Point const point)
 {
-	return ( ((a.getx() - point.getx()) * (b.gety() - point.gety())) - ((a.gety() - point.gety()) * (b.getx() - point.getx())) );
+	return (((a.getx() - point.getx()) * (b.gety() - point.gety())) - ((a.gety() - point.gety()) * (b.getx() - point.getx())));
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed	w1 = vector(a, b, point);
-	Fixed	w2 = vector(b, c, point);
-	Fixed	w3 = vector(c, a, point);
+	Fixed	edge1 = vector(a, b, point);
+	Fixed	edge2 = vector(b, c, point);
+	Fixed	edge3 = vector(c, a, point);
 
-	return ((w1 > 0 && w2 > 0 && w1 > 0) || (w1 < 0 && w2 < 0 && w1 < 0));
+	return ((edge1 > 0 && edge2 > 0 && edge3 > 0) || (edge1 < 0 && edge2 < 0 && edge3 < 0));
 }
--- Module_02/ex03/srcs/main.cpp
+++ Module_02/ex03/srcs/main.cpp
@@ -6,21 +6,99 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 08:15:43 by fllanet           #+#    #+#             */
-/*   Updated: 2023/11/28 09:19:30 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/11/28 10:48:24 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 #include "../includes/Point.hpp"
 
-int	main( void )
+int	main(void)
 {
-	// False
-	Point	a(1.0f, 0.0f), b(20.0f, 0.0f), c(0.0f, 20.0f), point(0.0f, 0.10f);
+	Point	a;
+	Point	b;
+	Point	c;
+	Point	p;
 
-	if (bsp(a, b, c, point))
-		std::cout << GREEN << "True" << WHITE << std::endl;
+	std::cout << std::endl;
+
+	// Test 1 : In => True
+	a = Point(0.0f, 0.0f);
+	b = Point(10.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(10.0f, 15.0f);
+	std::cout << YELLOW << "Test 1 : Point inside the triangle" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< GREEN << "True" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
 	else
-		std::cout << RED << "False" << WHITE << std::endl;
-	return 0;
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+	
+	// Test 2 : Out => False
+	a = Point(0.0f, 0.0f);
+	b = Point(10.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(30.0f, 15.0f);
+	std::cout << YELLOW << "Test 2 : Point outside the triangle" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< RED << "False" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
+	else
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+
+	// Test 3 : Corner => False
+	a = Point(0.0f, 0.0f);
+	b = Point(10.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(10.0f, 30.0f);
+	std::cout << YELLOW << "Test 3 : Point is a corner of the triangle" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< RED << "False" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
+	else
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+
+	// Test 4 : Edge => False
+	a = Point(0.0f, 0.0f);
+	b = Point(0.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(0.0f, 10.0f);
+	std::cout << YELLOW << "Test 4 : Point is an edge of the triangle" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< RED << "False" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
+	else
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+
+	// Test 5 : In Pixel Perfect => True
+	a = Point(0.0f, 0.0f);
+	b = Point(0.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(0.1f, 0.1f);
+	std::cout << YELLOW << "Test 5 : Point inside the triangle (Pixel Perfect)" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< GREEN << "True" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
+	else
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+	
+	// Test 6 : Out Pixel Perfect => False
+	a = Point(0.0f, 0.0f);
+	b = Point(20.0f, 30.0f);
+	c = Point(20.0f, 0.0f);
+	p = Point(20.1f, 0.1f);
+	std::cout << YELLOW << "Test 6 : Point outside the triangle (Pixel Perfect)" << WHITE << std::endl;
+	std::cout << BLUE << "A = (" << a.getx() << "," << a.gety() << "), B = (" << b.getx() << "," << b.gety() << "), C = (" << c.getx() << "," << c.gety() << "), P = (" << p.getx() << "," << p.gety() << ")\n" << WHITE << "Should be  : "<< RED << "False" << WHITE << std::endl;
+	
+	if (bsp(a, b, c, p))
+		std::cout << "BSP return : " << GREEN << "True" << WHITE << std::endl << std::endl;
+	else
+		std::cout << "BSP return : " << RED << "False" << WHITE << std::endl << std::endl;
+	
+	return (0);
 }
