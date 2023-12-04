7525d6ad70c1cac56d3e96561f1f73a4d40945c1
ffouquet42 <ffouquet42@gmail.com>
2023-12-04 12:26:52 +0100

    create basic setup for ex00

---
+++ Module_04/ex00/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 12:25:12 by fllanet           #+#    #+#             */
+/*   Updated: 2023/12/04 12:26:28 by fllanet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	...
+	return 0;
+}
