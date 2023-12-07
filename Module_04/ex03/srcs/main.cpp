dd091f744a4f6bfac644519d7042ac674cc24349
ffouquet42 <ffouquet42@gmail.com>
2023-12-07 16:34:08 +0100

    Module_04 updated

--- Module_04/ex03/srcs/main.cpp
+++ Module_04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 12:12:39 by fllanet           #+#    #+#             */
-/*   Updated: 2023/12/07 16:30:58 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/12/07 16:33:57 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ int main()
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
+	ICharacter* player1 = new Character("player1");
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
-	me->equip(tmp);
+	player1->equip(tmp);
 	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	ICharacter* player = new Character("player");
-	me->use(0, *player);
-	me->use(1, *player);
-	delete player;
-	delete me;
+	player1->equip(tmp);
+	ICharacter* player2 = new Character("player2");
+	player1->use(0, *player2);
+	player1->use(1, *player2);
+	delete player2;
+	delete player1;
 	delete src;
 	return 0;
 }
