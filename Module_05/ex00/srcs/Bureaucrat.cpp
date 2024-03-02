6505f92409b10d92a8c42a5c573b78d3441bc59c
ffouquet42 <ffouquet42@gmail.com>
2024-03-02 16:21:34 +0100

    start ex00

--- Module_05/ex00/srcs/Bureaucrat.cpp
+++ Module_05/ex00/srcs/Bureaucrat.cpp
@@ -6,7 +6,47 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:28:12 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/02 15:28:13 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/02 15:58:46 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Bureaucrat.hpp"
+
+// Constructor & Destructor
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	_grade = grade;
+	std::cout << GREEN << "Constructor called" << WHITE << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << RED << "Destructor called" << WHITE << std::endl; 
+}
+
+// Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade)
+// {
+// 	std::cout << GREEN << "Constructor called" << WHITE << std::endl;
+// }
+
+// Bureaucrat::Bureaucrat(const Bureaucrat &cpy)
+// {
+// 	*this = cpy;
+// 	std::cout << GREEN << "Constructor called" << WHITE << std::endl;
+// }
+
+// Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &cpy)
+// {
+// 	_name = cpy._name;
+// 	_grade = cpy._grade;
+// 	std::cout << GREEN << "Constructor called" << WHITE << std::endl;
+// 	return (*this);
+// }
+
+
+// Functions
--- Module_05/ex00/srcs/main.cpp
+++ Module_05/ex00/srcs/main.cpp
@@ -6,7 +6,18 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:28:09 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/02 15:28:10 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/02 16:02:05 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Bureaucrat.hpp"
+
+int main(void)
+{
+	std::cout << YELLOW << "Create Bureaucrat <a> with grade <1>" << WHITE << std::endl;
+	Bureaucrat a("a", 1);
+	// get name and grade
+	
+	
+	return (0);
+}
