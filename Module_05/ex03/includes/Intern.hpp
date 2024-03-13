66721e8a89cf12302d7fd44c3e6e8c834db6f865
ffouquet42 <ffouquet42@gmail.com>
2024-03-13 02:44:43 +0100

    Intern create, need tests in main.cpp

--- Module_05/ex03/includes/Intern.hpp
+++ Module_05/ex03/includes/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 00:35:24 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/13 00:40:00 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/13 00:48:19 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ class Intern
 		Intern(const Intern &cpy);
 		Intern &operator=(const Intern &cpy);
 
-		AForm *makeForm(std::string formName, std::string formTarget);
+		AForm *makeForm(std::string name, std::string target);
 	private:
 };
--- Module_05/ex03/srcs/Intern.cpp
+++ Module_05/ex03/srcs/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 00:35:21 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/13 00:44:12 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/13 02:41:59 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ Intern::Intern(const Intern &cpy)
 
 Intern	&Intern::operator=(const Intern &cpy)
 {
+	(void)cpy;
 	std::cout << GREEN << "++ Intern copy assignment operator called" << WHITE << std::endl;
 	return (*this);
 }
@@ -39,7 +40,34 @@ Intern	&Intern::operator=(const Intern &cpy)
 
 //---------------   Functions   ---------------//
 
-AForm	*Intern::makeForm(std::string formName, std::string formTarget)
+AForm	*newShrubbery(std::string target)
 {
+	return (new ShrubberyCreationForm(target)); // new? leaks? delete?
+}
+
+AForm	*newRobotomy(std::string target)
+{
+	return (new RobotomyRequestForm(target)); // new? leaks? delete?
+}
+
+AForm	*newPresidential(std::string target)
+{
+	return (new PresidentialPardonForm(target)); // new? leaks? delete?
+}
+
+AForm	*Intern::makeForm(std::string name, std::string target)
+{
+	AForm *newForm = NULL;
+	AForm *(*f[3])(std::string target) = { &newShrubbery, &newRobotomy, &newPresidential };
+	std::string form_list[3] = { "shrubbery creation", "robotomy request", "presidential pardon" };
+	int id = -1;
 	
+	for (int i = 0; i < 3; i++)
+		if (form_list[i] == name)
+			id = i;
+	if (id == -1)
+		return (std::cout << RED << "Invalid name" << WHITE << std::endl, newForm); // color
+	return (std::cout << GREEN << "Intern creates " << form_list[id] << WHITE << std::endl, f[id](target)); // color
+
+	// return (id == -1) ? (std::cout << RED << "Invalid name" << WHITE << std::endl, nullptr) : (std::cout << GREEN << "Intern creates " << form_list[id] << WHITE << std::endl, f[id](target);
 }
