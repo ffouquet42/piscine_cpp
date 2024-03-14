2bf10cbcc12a33108d9a90c9a58c91512af36003
ffouquet42 <ffouquet42@gmail.com>
2024-03-14 23:22:08 +0100

    test invalid name done

--- Module_05/ex03/srcs/main.cpp
+++ Module_05/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:28:09 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/14 23:05:37 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/14 23:16:30 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,41 +27,51 @@ int main(void)
 		Bureaucrat A("A", 1);
 		std::cout << A << std::endl;
 	
-		std::cout << YELLOW << "\nTest #003 : Intern make form <shrubbery creation>" << WHITE << std::endl;
+	
+		std::cout << YELLOW << "\nTest #003 : Intern try to make form <shrubery creation>" << WHITE << std::endl;
+		intern.makeForm("shrubery creation", "forest");
+
+		std::cout << YELLOW << "Test #004 : Intern make form <shrubbery creation>" << WHITE << std::endl;
 		AForm *F = intern.makeForm("shrubbery creation", "forest");
 		
-		std::cout << YELLOW << "Test #004 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #005 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
 		std::cout << *F << std::endl;
 		A.signForm(*F);
 		std::cout << *F << std::endl;
 
-		std::cout << YELLOW << "Test #005 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #006 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
 		A.executeForm(*F);
 
 		delete F;
 		
-		std::cout << YELLOW << "\nTest #006 : Intern make form <robotomy request>" << WHITE << std::endl;
+		std::cout << YELLOW << "\nTest #007 : Intern try to make form <robotomi request>" << WHITE << std::endl;
+		intern.makeForm("robotomi request", "robot");
+		
+		std::cout << YELLOW << "Test #008 : Intern make form <robotomy request>" << WHITE << std::endl;
 		AForm *G = intern.makeForm("robotomy request", "robot");
 
-		std::cout << YELLOW << "Test #007 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #009 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
 		std::cout << *G << std::endl;
 		A.signForm(*G);
 		std::cout << *G << std::endl;
 
-		std::cout << YELLOW << "Test #008 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #010 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
 		A.executeForm(*G);
 
 		delete G;
 
-		std::cout << YELLOW << "\nTest #009 : Intern make form <presidential pardon>" << WHITE << std::endl;
+		std::cout << YELLOW << "\nTest #011 : Intern try to make form <prezidential pardon>" << WHITE << std::endl;
+		intern.makeForm("prezidential pardon", "pardon");
+		
+		std::cout << YELLOW << "Test #012 : Intern make form <presidential pardon>" << WHITE << std::endl;
 		AForm *H = intern.makeForm("presidential pardon", "pardon");
 
-		std::cout << YELLOW << "Test #010 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #013 : Bureaucrat <A> sign form create by the Intern" << WHITE << std::endl;
 		std::cout << *H << std::endl;
 		A.signForm(*H);
 		std::cout << *H << std::endl;
 
-		std::cout << YELLOW << "Test #011 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
+		std::cout << YELLOW << "Test #014 : Bureaucrat <A> execute form create by the Intern" << WHITE << std::endl;
 		A.executeForm(*H);
 
 		delete H;
@@ -71,6 +81,8 @@ int main(void)
 	} catch(Bureaucrat::GradeTooLowException &e) {
 		std::cout << MAGENTA << e.tooLow() << WHITE << std::endl;
 	}
+
+	// Try : Intern try to create form with invalid name
 
 	return (0);
 }
