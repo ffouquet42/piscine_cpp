10cad43bfb458d39c65da61350cf553d0ed8212a
ffouquet42 <ffouquet42@gmail.com>
2024-03-18 02:36:11 +0100

    ex02 done

--- Module_07/ex02/includes/Array.hpp
+++ Module_07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 00:27:11 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/18 00:28:00 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/18 02:30:55 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,51 @@
 
 
 //---------------   templates   ---------------// + class ?
+template <typename T>
+class Array
+{
+	public:
+		Array() : _arr(new T[0]), _len(0) {}
+		Array(int n) : _arr(new T[n]), _len(n) {
+			for (int i = 0; i < n; i++)
+				_arr[i] = 0;
+		}
+		Array(Array const &cpy) : _arr(new T[cpy._len]), _len(cpy._len) {
+			for (int i = 0; i < _len; i++)
+				_arr[i] = cpy._arr[i];
+		}
+		~Array() { delete[] _arr; }
 
+		Array<T> &operator=(Array<T> const &cpy) {
+			if (this != &cpy)
+			{
+				delete[] _arr;
+				_len = cpy._len;
+				_arr =  new T[_len];
+				for (int i = 0; i < _len; i++)
+					_arr[i] = cpy._arr[i];
+			}
+			return (*this);
+		}
 
+		T &operator[](int index) {
+			if (index >= _len)
+				throw InvalidIndex();
+			return (_arr[index]);
+		}
+
+		class InvalidIndex : public std::exception {
+			const char *what() const throw() {
+				return ("=> Invalid Index");
+			}
+		};
+
+		int size() { return (_len); }
+
+	private:
+		T *_arr;
+		int _len;
+};
 
 
 //---------------   colors   ---------------//
--- Module_07/ex02/srcs/main.cpp
+++ Module_07/ex02/srcs/main.cpp
@@ -6,15 +6,73 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 00:27:14 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/18 00:28:25 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/18 02:35:34 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Array.hpp"
 
-int main(void)
+int main(void) 
 {
-	
+	{
+		std::cout << YELLOW << "\nTest #001 - Array(5) <int>" << WHITE << std::endl;
+		Array<int> arr(5);
+		try {
+			for (int i = 0; i < 5; i++)
+				std::cout << GREEN << arr[i] << WHITE << std::endl;
+		} catch (std::exception &e) {
+			std::cout << MAGENTA << e.what() << WHITE << std::endl;
+		}
+	}
+
+	{
+		std::cout << YELLOW << "\nTest #002 - Array(20) <int>" << WHITE << std::endl;
+		Array<int> arr(20);
+		try {
+			for (int i = 0; i < 20; i++)
+				std::cout << GREEN << arr[i] << WHITE << std::endl;
+		} catch (std::exception &e) {
+			std::cout << MAGENTA << e.what() << WHITE << std::endl;
+		}
+	}
+
+	{
+		std::cout << YELLOW << "\nTest #003 - Array(5) <int> | Try to display unexisting arr[i]" << WHITE << std::endl;
+		Array<int> arr(5);
+		try {
+			for (int i = 0; i < 10; i++)
+				std::cout << GREEN << arr[i] << WHITE << std::endl;
+		} catch (std::exception &e) {
+			std::cout << MAGENTA << e.what() << WHITE << std::endl;
+		}
+	}
+
+	{
+		std::cout << YELLOW << "\nTest #004 - Array(5) <char>" << WHITE << std::endl;
+		Array<char> arr(5);
+		try {
+			for (int i = 0; i < 5; i++)
+				std::cout << GREEN << "\"" << arr[i] << "\"" << WHITE << std::endl;
+		} catch (std::exception &e) {
+			std::cout << MAGENTA << e.what() << WHITE << std::endl;
+		}
+	}
+
+	{
+		std::cout << YELLOW << "\nTest #005 - Array(17) <char>" << WHITE << std::endl;
+		Array<char> arr(17);
+		try {
+			for (int i = 0; i < 17; i++)
+				std::cout << GREEN << "\"" << arr[i] << "\"" << WHITE << std::endl;
+		} catch (std::exception &e) {
+			std::cout << MAGENTA << e.what() << WHITE << std::endl;
+		}
+	}
+
+	// { // leaks
+	// 	int *a = new int();
+	// 	std::cout << a << std::endl;
+	// }
 	
 	return (0);
 }
