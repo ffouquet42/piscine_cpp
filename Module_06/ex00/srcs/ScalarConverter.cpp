c28d39b3c539379785b1a087b3f31c4ea4d83640
ffouquet42 <ffouquet42@gmail.com>
2024-03-19 22:58:04 +0100

    ex00 OK

--- Module_06/ex00/srcs/ScalarConverter.cpp
+++ Module_06/ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 18:19:41 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/19 22:36:29 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/19 22:54:34 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,25 @@ int	is_number(char c)
 void	display_char(const char *str, double str_as_double)
 {
 	std::cout << BLUE << "\nChar  : " << WHITE;
-	if (str && str[0] && !str[1] && !is_number(str[0]))
-		std::cout << "\'" << str[0] << "\'";
-	else if (str_as_double <= 31 && str_as_double >= 0)
-		std::cout << "Non displayable";
+	if (str && str[0] && !is_number(str[0]) && !str[1])
+		std::cout << BLUE << "\'" << str[0] << "\'" << WHITE << std::endl;
+	else if (str_as_double < 32 && str_as_double >= 0)
+		std::cout << CYAN << "Non displayable" << WHITE << std::endl;
 	else if (str_as_double < 0 || str_as_double >= 127 || !(str_as_double >= 0 || str_as_double <= 0))
-		std::cout << "Impossible";
+		std::cout << CYAN << "Impossible" << WHITE << std::endl;
 	else
-		std::cout << "\'" << static_cast<char>(str_as_double) << "\'";
-	std::cout << std::endl;
+		std::cout << BLUE << "\'" << static_cast<char>(str_as_double) << "\'" << WHITE << std::endl;
 }
 
 void	display_int(const char *str, double str_as_double)
 {
 	std::cout << BLUE << "Int   : " << WHITE;
-	int	res = static_cast<int>(str_as_double);
-	if (!(str_as_double >= 0 || str_as_double <= 0))
-		std::cout << "Impossible";
-	else if (str_as_double < -2147483648 || str_as_double > 2147483647)
-		std::cout << "Impossible";
+	if ((!(str_as_double >= 0 || str_as_double <= 0)) || (str_as_double < -2147483648 || str_as_double > 2147483647))
+		std::cout << CYAN << "Impossible" << WHITE << std::endl;
 	else if (str && str[0] && !is_number(str[0]) && !str[1])
-		std::cout << static_cast<int>(str[0]);
+		std::cout << BLUE << static_cast<int>(str[0]) << WHITE << std::endl;
 	else
-		std::cout << res;
-	std::cout << std::endl;
+		std::cout << BLUE << static_cast<int>(str_as_double) << WHITE << std::endl;
 }
 
 void	display_float(const char *str, double str_as_double)
@@ -78,9 +73,6 @@ void	ScalarConverter::convert(std::string str)
 {
 	const char *str_as_char = str.c_str();
 	double str_as_double = std::strtod(str_as_char, 0);
-
-	std::cout << MAGENTA << "str = " << str_as_char << WHITE << std::endl;
-	std::cout << MAGENTA << "nb  = " << str_as_double << WHITE << std::endl;
 
 	display_char(str_as_char, str_as_double);
 	display_int(str_as_char, str_as_double);
