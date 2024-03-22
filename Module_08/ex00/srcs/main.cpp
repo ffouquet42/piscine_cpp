f45e544b6b6587d8b8375c550bd5554223f2bd13
ffouquet42 <ffouquet42@gmail.com>
2024-03-22 23:57:08 +0100

    precise pos where value was find first

--- Module_08/ex00/srcs/main.cpp
+++ Module_08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 12:32:04 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/22 23:24:52 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/22 23:54:24 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,21 @@
 int	main()
 {
 	std::cout << YELLOW << "\nTest #001 - Create vector<int>" << WHITE << std::endl;
-	std::cout << BLUE << "vector<int> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17]" << WHITE << std::endl;
+	std::cout << BLUE << "vector<int> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 5, 10, 17]" << WHITE << std::endl;
 	
 	std::vector<int> arr;
 	for (int i = 0; i <= 10; i++)
 		arr.push_back(i);
+	arr.push_back(17);
+	arr.push_back(5);
+	arr.push_back(10);
 	arr.push_back(17);
 	
 	{
 		try {
 			std::cout << YELLOW << "\nTest #002 - Search 0" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 0);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
@@ -36,7 +39,7 @@ int	main()
 		try {
 			std::cout << YELLOW << "\nTest #003 - Search 5" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 5);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
@@ -46,7 +49,7 @@ int	main()
 		try {
 			std::cout << YELLOW << "\nTest #004 - Search 10" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 10);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
@@ -56,7 +59,7 @@ int	main()
 		try {
 			std::cout << YELLOW << "\nTest #005 - Search 15" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 15);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
@@ -66,7 +69,7 @@ int	main()
 		try {
 			std::cout << YELLOW << "\nTest #006 - Search 17" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 17);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
@@ -76,7 +79,7 @@ int	main()
 		try {
 			std::cout << YELLOW << "\nTest #007 - Search 20" << WHITE << std::endl;
 			std::vector<int>::iterator it = easyfind(arr, 20);
-			std::cout << GREEN << "Value " << *it << " found" << WHITE << std::endl;
+			std::cout << GREEN << "Value " << *it << " found at pos " << std::distance(arr.begin(), it) << WHITE << std::endl;
 		} catch (std::exception &e) {
 			std::cout << MAGENTA << e.what() << WHITE << std::endl;
 		}
