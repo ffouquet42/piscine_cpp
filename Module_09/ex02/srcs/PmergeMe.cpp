44ed46f710985bfd178b9b1842d4259b61bf9396
ffouquet42 <ffouquet42@gmail.com>
2024-03-24 03:26:57 +0100

    setup ex02

---
+++ Module_09/ex02/srcs/PmergeMe.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 15:28:09 by fllanet           #+#    #+#             */
+/*   Updated: 2024/03/24 03:26:44 by fllanet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/PmergeMe.hpp"
+
+//---------------   Constructors & Destructors   ---------------//
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe &cpy) {}
+
+PmergeMe	&PmergeMe::operator=(const PmergeMe &cpy) {
+	if (this != &cpy) {
+		
+	}
+	return (*this);
+}
+
+
+//---------------   Functions   ---------------//
+
