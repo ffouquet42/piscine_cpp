515c3b7e2ae6694872f9688263b54bb2e5679a53
ffouquet42 <ffouquet42@gmail.com>
2024-03-24 04:39:19 +0100

    update main.cpp

--- Module_09/ex00/srcs/main.cpp
+++ Module_09/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:28:09 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/24 02:10:29 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/03/24 04:37:30 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int main(int argc, char **argv)
 	}
 
 	BitcoinExchange btc;
-	btc.loadDatabase();
-	btc.calculBtcValue(in);
+	btc.load_database();
+	btc.calcul_btc_value(in);
 
 	return (0);
 }
